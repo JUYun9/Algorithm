37e738e4828d7e62cfb6d0db8d41f9e0b876e54e
JEONG U YUN <dbswjddn6@naver.com>
2024-07-21 16:07:50 +0900

    create practice.cpp

---
+++ practice.cpp
@@ -0,0 +1,14 @@
+﻿'Project1.exe'(Win32): 'C:\Users\dbswj\source\repos\Project1\x64\Debug\Project1.exe'을(를) 로드했습니다. 기호가 로드되었습니다.
+'Project1.exe'(Win32): 'C:\Windows\System32\ntdll.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+'Project1.exe'(Win32): 'C:\Windows\System32\kernel32.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+'Project1.exe'(Win32): 'C:\Windows\System32\KernelBase.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+'Project1.exe'(Win32): 'C:\Windows\System32\msvcp140d.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+'Project1.exe'(Win32): 'C:\Windows\System32\vcruntime140_1d.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+'Project1.exe'(Win32): 'C:\Windows\System32\vcruntime140d.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+'Project1.exe'(Win32): 'C:\Windows\System32\ucrtbased.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+2988 스레드가 종료되었습니다(코드: 0 (0x0)).
+'Project1.exe'(Win32): 'C:\Windows\System32\kernel.appcore.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+'Project1.exe'(Win32): 'C:\Windows\System32\msvcrt.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+23332 스레드가 종료되었습니다(코드: 0 (0x0)).
+9012 스레드가 종료되었습니다(코드: 0 (0x0)).
+'[16540] Project1.exe' 프로그램이 종료되었습니다(코드: 0 (0x0)).
