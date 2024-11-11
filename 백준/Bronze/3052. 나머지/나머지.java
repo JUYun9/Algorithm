79db743a09d53a566bb12d729422bfe94e9f4f86
윤정우 <124337134+JUYun9@users.noreply.github.com>
2024-11-11 16:58:17 +0900

    [Bronze II] Title: 나머지, Time: 172 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[11];
+        int[] count = new int[11];
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+            count[i] = arr[i] % 42;
+        }
+
+        for (int j = 9; j > 0; j--) {
+            for (int i = 0; i < j; i++) {
+                if (count[i] > count[i + 1]) {
+                    int tmp = count[i];
+                    count[i] = count[i + 1];
+                    count[i + 1] = tmp;
+                }
+            }
+        }
+
+        int cnt = 1;
+        for (int i = 0; i < 9; i++) {
+            if (count[i] != count[i + 1])
+                cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
