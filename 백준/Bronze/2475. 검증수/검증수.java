5812847f1a9851c72d8f652fc134d96442f5264f
윤정우 <124337134+JUYun9@users.noreply.github.com>
2024-11-16 04:09:06 +0900

    [Bronze V] Title: 검증수, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+
+        int result = (a*a + b*b + c*c + d*d + e*e) % 10;
+        bw.write(String.valueOf(result));
+        bw.flush();
+
+        bw.close();
+    }
+}
