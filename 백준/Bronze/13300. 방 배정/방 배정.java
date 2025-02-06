e4c3a15cec86729fe33561a8ef0a6f0a3cd5db00
윤정우 <124337134+JUYun9@users.noreply.github.com>
2025-02-06 14:18:23 +0900

    [Bronze II] Title: 방 배정, Time: 92 ms, Memory: 14064 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[7][2];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int S = Integer.parseInt(st.nextToken());
+            int Y = Integer.parseInt(st.nextToken());
+
+            arr[Y][S]++;
+        }
+
+        int result = 0;
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 2; j++)
+                result += (arr[i][j]+1) / 2;
+        }
+
+        bw.write(String.valueOf(result));
+
+        bw.flush();
+        bw.close();
+    }
+}
