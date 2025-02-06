d16c820e79e73925f567d4040b36a52456959b80
윤정우 <124337134+JUYun9@users.noreply.github.com>
2025-02-06 13:59:25 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,5 +1,5 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -7,18 +7,20 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
         for (int i = 0; i < N; i++)
             arr[i] = Integer.parseInt(st.nextToken());
 
-        int v = Integer.parseInt(br.readLine());
-        int cnt = 0;
-        for (int s: arr) {
-            if (s == v)
-                cnt++;
+        int target = Integer.parseInt(br.readLine());
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            if (arr[i] == target)
+                count++;
         }
-        bw.write(String.valueOf(cnt));
+
+        bw.write(String.valueOf(count));
+
         bw.flush();
         bw.close();
     }
