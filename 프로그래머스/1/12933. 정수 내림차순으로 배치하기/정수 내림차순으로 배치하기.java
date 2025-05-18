3e532a3249c24b5f81c619d926444cee05870dc7
윤정우 <124337134+JUYun9@users.noreply.github.com>
2025-05-19 00:36:45 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.62 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -3,18 +3,33 @@ import java.util.*;
 class Solution {
     public long solution(long n) {
         long answer = 0;
-        String str = String.valueOf(n);
-        int[] arr = new int[str.length()];
-        for (int i = 0; i < str.length(); i++)
-            arr[i] = str.charAt(i) - '0';
+        
+        Queue<Long> q = new LinkedList<>();
+        
+        while (n != 0) {
+            q.add(n%10);
+            n /= 10;
+        }
+        
+        int size = q.size();
+        long[] arr = new long[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = q.peek();
+            q.poll();
+        }
         
         Arrays.sort(arr);
-       
-        long j = 1;
+        
+        int k = 1;
         for (int i = 0; i < arr.length; i++) {
-            answer += arr[i] * j;
-            j *= 10;
+            if (arr[i] == 0)
+                answer *= 10;
+            else {
+                answer += arr[i] * k;
+            }
+            k *= 10;
         }
+        
         return answer;
     }
 }
