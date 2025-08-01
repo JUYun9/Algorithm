43441cf9f42064b6d87fbf7fe74a6973df984435
윤정우 <124337134+JUYun9@users.noreply.github.com>
2025-08-01 15:26:23 +0900

    [Silver III] Title: N과 M (4), Time: 132 ms, Memory: 16920 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int n, m;
+    private static int[] arr;
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+
+        backtracking(0);
+
+        System.out.println(sb);
+    }
+
+    private static void backtracking(int count) {
+        if (m == count) {
+            for (int i = 0; i < m; i++)
+                sb.append(arr[i]).append(" ");
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (count > 0) {
+                if (arr[count - 1] > i)
+                    continue;
+            }
+
+            arr[count] = i;
+            backtracking(count + 1);
+        }
+
+    }
+}
