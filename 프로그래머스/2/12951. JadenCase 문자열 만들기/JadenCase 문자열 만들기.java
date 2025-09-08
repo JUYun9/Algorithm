7826cde4edc909e47c5ee013517f92bbc51450a8
윤정우 <124337134+JUYun9@users.noreply.github.com>
2025-09-08 15:53:27 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.15 ms, Memory: 71.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        StringBuilder answer = new StringBuilder();
+        
+        if (!Character.isDigit(s.charAt(0)))
+            answer.append(Character.toUpperCase(s.charAt(0)));
+        else
+            answer.append(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ' ')
+                answer.append(" ");
+            else if (s.charAt(i-1) == ' ' && !Character.isDigit(s.charAt(i)))
+                answer.append(Character.toUpperCase(s.charAt(i)));
+            else if (s.charAt(i-1) != ' ' && !Character.isDigit(s.charAt(i)))
+                answer.append(Character.toLowerCase(s.charAt(i)));
+            else
+                answer.append(s.charAt(i));
+        }
+        
+        return answer.toString();
+    }
+}
