4faf850d82f01575976eb74c78290f170e3d4847
윤정우 <124337134+JUYun9@users.noreply.github.com>
2025-11-28 18:39:33 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1320 ms, Memory: 58552 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testCase = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        List<Axis> list = new ArrayList<>();
+        for (int i = 0; i < testCase; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            list.add(new Axis(x, y));
+        }
+
+        Collections.sort(list);
+
+        for (int i = 0; i < list.size(); i++)
+            System.out.println(list.get(i).x + " " + list.get(i).y);
+
+    }
+}
+
+class Axis implements Comparable<Axis> {
+    int x;
+    int y;
+
+    public Axis(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Axis other) {
+        if (this.x != other.x) {
+            if (this.x > other.x)
+                return 1;
+            else
+                return -1;
+        } else {
+            if (this.y > other.y)
+                return 1;
+            else
+                return -1;
+        }
+    }
+}
