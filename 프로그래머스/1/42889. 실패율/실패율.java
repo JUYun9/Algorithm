b5796f0c55a9dfeb708f52ecadcf82c0a7e9ce63
윤정우 <124337134+JUYun9@users.noreply.github.com>
2025-11-28 17:44:08 +0900

    [level 1] Title: 실패율, Time: 5.06 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -2,58 +2,56 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
+        int totalMemberCnt = stages.length;
         
-        int[] failCount = new int[N+2];
-        
-        int totalMemberCount = stages.length;
+        // 인덱스: 라운드
+        // 값: 해당 인덱스에서 실패한 멤버의 수
+        int[] failCnt = new int[N+2];
         for (int i = 0; i < stages.length; i++) {
             int target = stages[i];
-            failCount[target]++;   // (target-1) 라운드의 실패한 사용자의 수가 담긴다.
+            failCnt[target]++;
         }
         
         List<StageInfo> failPercent = new ArrayList<>();
-        for (int i = 1; i < failCount.length-1; i++) {
-            double rating = 0.0;
+        for (int i = 1; i < failCnt.length-1; i++) {
+            if (totalMemberCnt < failCnt[i])
+                break;
             
-            if (totalMemberCount > 0) {
-                rating = (double) failCount[i] / totalMemberCount;
-                totalMemberCount -= failCount[i];
-            }
+            double target = (double) failCnt[i] / totalMemberCnt;
+            totalMemberCnt -= failCnt[i];
             
-            failPercent.add(new StageInfo(i, rating));
+            failPercent.add(new StageInfo(i, target));
         }
         
         Collections.sort(failPercent);
         
         int[] answer = new int[N];
-        for (int i = 0; i < N; i++)
-            answer[i] = failPercent.get(i).stageNum;
-        
+        for (int i = 0; i < N; i++) {
+            answer[i] = failPercent.get(i).round;
+        }
+    
+    
         return answer;
     }
 }
 
+// Comparable<..>
 class StageInfo implements Comparable<StageInfo> {
-    int stageNum;
+    int round;
     double rate;
-
-    public StageInfo(int stageNum, double rate) {
-        this.stageNum = stageNum;
+    
+    StageInfo(int round, double rate) {
+        this.round = round;
         this.rate = rate;
     }
-
+    
     @Override
-    public int compareTo(StageInfo other) {
-        if (this.rate < other.rate)
-            return 1;
-        if (this.rate > other.rate)
+    public int compareTo(StageInfo o) {
+        if (this.rate > o.rate)
             return -1;
+        else if (this.rate < o.rate)
+            return 1;
         
-        return Integer.compare(this.stageNum, other.stageNum);
+        return Integer.compare(this.round, o.round);
     }
 }
-
-// 스테이지 배열에 적혀있는 숫자 = 해당 라운드를 통과하지 못했다.
-// 스테이지 배열에서 가장 큰 숫자 크기만큼의 배열(a)을 선언한다.
-// a[]에는 각 스테이지 실패율 값과 인덱스 번호를 넣는다.
-// a[]를 내림차순으로 정렬한 값을 출력한다.
