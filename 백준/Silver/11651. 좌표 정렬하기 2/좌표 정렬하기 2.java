8359a3e9fc9f0ac35ccc029dd194e9a32f259afa
윤정우 <124337134+JUYun9@users.noreply.github.com>
2025-12-01 16:04:01 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1228 ms, Memory: 58564 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+// 2025-12-01
+// 15:55 ~
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testCase = Integer.parseInt(br.readLine());
+        List<Point> points = new ArrayList<>();
+        for (int i = 0; i < testCase; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            points.add(new Point(x, y));
+        }
+
+        Collections.sort(points);
+
+        for (int i = 0; i < points.size(); i++) {
+            System.out.println(points.get(i).x + " " + points.get(i).y);
+        }
+    }
+}
+
+class Point implements Comparable<Point> {
+    int x;
+    int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Point other) {
+        if (this.y > other.y)
+            return 1;
+        else if (this.y < other.y)
+            return -1;
+        else {
+            if (this.x > other.x)
+                return 1;
+            else if (this.x < other.x)
+                return -1;
+            return 0;
+        }
+    }
+}
