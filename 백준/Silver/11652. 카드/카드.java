cefab5204e297d8eaf5c33b0acc0738ff4ddbdeb
윤정우 <124337134+JUYun9@users.noreply.github.com>
2025-12-04 15:07:07 +0900

    [Silver IV] Title: 카드, Time: 264 ms, Memory: 25640 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -1,41 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Map<Long, Integer> map = new HashMap<>();
-
         int testCase = Integer.parseInt(br.readLine());
-        while (testCase-- > 0) {
-            Long num = Long.parseLong(br.readLine());
+        long[] nums = new long[testCase];
 
-            if (map.get(num) == null)
-                map.put(num, 1);
-            else
-                map.put(num, map.get(num)+1);
+        int k = 0;
+        while (testCase-- > 0) {
+            long target = Long.parseLong(br.readLine());
+            nums[k++] = target;
         }
 
-        Long maxKey = 0L;
-        int maxValue = Integer.MIN_VALUE;
-        for (Long key: map.keySet()) {
-            int targetValue = map.get(key);
-            if (targetValue > maxValue) {
-                maxValue = targetValue;
-                maxKey = key;
-            } else if (targetValue == maxValue) {
-                if (maxKey > key) {
-                    maxKey = key;
+        Arrays.sort(nums);
+
+        int cnt = 1;
+        int maxCnt = 1;
+        long minNumber = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i-1]) {
+                cnt++;
+            } else {
+                if (cnt > maxCnt) {
+                    maxCnt = cnt;
+                    minNumber = nums[i-1];
+                } else if (cnt == maxCnt) {
+                    if (minNumber > nums[i-1]) {
+                        minNumber = nums[i-1];
+                        maxCnt = cnt;
+                    }
                 }
+
+                cnt = 1;
             }
         }
 
-        System.out.println(maxKey);
+        if (cnt > maxCnt)
+            minNumber = nums[nums.length-1];
+
+        System.out.println(minNumber);
 
     }
 }
