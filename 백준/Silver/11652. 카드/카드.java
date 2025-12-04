257082ef8efe2b500de568975afdf567a438c0a3
윤정우 <124337134+JUYun9@users.noreply.github.com>
2025-12-04 14:25:38 +0900

    [Silver IV] Title: 카드, Time: 340 ms, Memory: 31312 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -4,36 +4,33 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
-// 2025-12-02
-// 12:42 ~ 13:10
-
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int num = Integer.parseInt(br.readLine());
         Map<Long, Integer> map = new HashMap<>();
-        for (int i = 0; i < num; i++) {
-            Long target = Long.valueOf(br.readLine());
-            if (map.get(target) != null)
-                map.put(target, map.get(target) + 1);
+
+        int testCase = Integer.parseInt(br.readLine());
+        while (testCase-- > 0) {
+            Long num = Long.parseLong(br.readLine());
+
+            if (map.get(num) == null)
+                map.put(num, 1);
             else
-                map.put(target, 1);
+                map.put(num, map.get(num)+1);
         }
 
         Long maxKey = 0L;
         int maxValue = Integer.MIN_VALUE;
         for (Long key: map.keySet()) {
-            int value = map.get(key);
-            if (value > maxValue) {
+            int targetValue = map.get(key);
+            if (targetValue > maxValue) {
+                maxValue = targetValue;
                 maxKey = key;
-                maxValue = value;
-            }
-            else if (value == maxValue) {
+            } else if (targetValue == maxValue) {
                 if (maxKey > key) {
                     maxKey = key;
-                    maxValue = value;
                 }
             }
         }
