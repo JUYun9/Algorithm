05ebd4fb631e08a5f877f604d387f509c1f4e7d4
윤정우 <124337134+JUYun9@users.noreply.github.com>
2026-01-03 15:19:36 +0900

    [Silver IV] Title: 수 찾기, Time: 560 ms, Memory: 47764 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,30 +12,26 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int num1 = Integer.parseInt(br.readLine());
-        HashMap<Integer, Integer> map1 = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         while (st.hasMoreElements()) {
             int target = Integer.parseInt(st.nextToken());
-            if (!map1.containsKey(target))
-                map1.put(target, 1);
+            set.add(target);
         }
 
         int num2 = Integer.parseInt(br.readLine());
-        int[] arr2 = new int[num2];
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < num2; i++)
-            arr2[i] = Integer.parseInt(st.nextToken());
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < num2; i++) {
-            if (map1.containsKey(arr2[i]))
+            int target = Integer.parseInt(st.nextToken());
+            if (set.contains(target))
                 sb.append(1).append("\n");
             else
                 sb.append(0).append("\n");
         }
-
 
         System.out.println(sb.toString());
     }
