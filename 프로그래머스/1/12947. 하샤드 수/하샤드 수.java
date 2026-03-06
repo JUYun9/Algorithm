f77b9e2de74e3747137f2671f1442667ecfec276
윤정우 <124337134+JUYun9@users.noreply.github.com>
2026-03-06 18:19:19 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -2,16 +2,14 @@ class Solution {
     public boolean solution(int x) {
         boolean answer = true;
         
-        int y = x;
         int sum = 0;
+        int y = x;
         while (y > 0) {
             sum += y%10;
             y /= 10;
         }
         
-       // answer = x/sum = 0 ? true : false;
-        if (x%sum != 0)
-            answer = false;
+        answer = (x%sum == 0) ? true : false;
         
         return answer;
     }
