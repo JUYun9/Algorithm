ca050e3d021fce028c31c10542139aa79f067126
윤정우 <124337134+JUYun9@users.noreply.github.com>
2026-03-06 15:52:36 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,20 +1,15 @@
-// 01:24 ~
-
 class Solution {
-    public int solution(long num) {
+    public int solution(int num) {
         int answer = 0;
+
+        long n = num;
         
-        while (num != 1) {
-            if (answer >= 500)
-                return -1;
-            
-            if (num%2 == 0)
-                num /= 2;
-            else
-                num = num*3 + 1;
-            
+        while (n != 1 && answer <= 500) {
+            n = (n%2 == 0) ? (n/2) : (3*n + 1);
             answer++;
         }
+        
+        answer = (answer == 501) ? -1 : answer;
         
         return answer;
     }
