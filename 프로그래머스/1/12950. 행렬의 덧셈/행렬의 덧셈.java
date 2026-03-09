a10956596b459ee1024b5fc05f5744892232c0fb
윤정우 <124337134+JUYun9@users.noreply.github.com>
2026-03-09 15:01:22 +0900

    [level 1] Title: 행렬의 덧셈, Time: 7.37 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -2,11 +2,9 @@ class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
         int[][] answer = new int[arr1.length][arr1[0].length];
         
-        // 행
-        for (int i = 0; i < arr1[0].length; i++) {
-            // 열
-            for (int j = 0; j < arr1.length; j++) {
-                answer[j][i] = arr1[j][i] + arr2[j][i];
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[0].length; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
             }
         }
         
