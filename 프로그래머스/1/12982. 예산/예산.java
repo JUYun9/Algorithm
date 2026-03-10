c85f524d1f59c5a6ee3eecf92178a05164901f1a
윤정우 <124337134+JUYun9@users.noreply.github.com>
2026-03-10 11:54:21 +0900

    [level 1] Title: 예산, Time: 1.21 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,6 +1,3 @@
-// d: 부서별 신청한 금액
-// budget: 예산
-// answer: 최대 개수
 import java.util.*;
 
 class Solution {
@@ -9,18 +6,13 @@ class Solution {
         
         Arrays.sort(d);
         
-        int count = 0;
-        for (int i = 0; i < d.length; i++) {
-            answer += d[i];
-            count++;
-            if (answer > budget)
-                return count-1;
+        for (int index: d) {
+            budget -= index;
+            if (budget < 0)
+                break;
+            answer++;
         }
         
-            
-        return count;
+        return answer;
     }
 }
-
-// sol1) 정렬
-// sol2) 브루트 포스. 재귀
