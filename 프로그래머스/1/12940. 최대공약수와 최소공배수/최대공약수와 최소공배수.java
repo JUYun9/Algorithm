e7dedfeb2d0af561d92a9c27ab7168cc80415cd1
윤정우 <124337134+JUYun9@users.noreply.github.com>
2026-03-10 09:51:17 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -2,34 +2,24 @@ class Solution {
     public int[] solution(int n, int m) {
         int[] answer = new int[2];
         
-
-        // 최대공약수
-        for (int i = n; i >= 1; i--) {
-            if (n%i == 0 && m%i == 0) {
-                answer[0] = i;
-                break;
-            }
-        }
+        int gcd = gcd(m, n);
+        answer[0] = gcd;
         
-        // 최소공배수
-        for (int i = 1; i <= n; i++) {
-            int k = m*i;
-            if (k%n == 0) {
-                answer[1] = k;
-                break;
-            }
-        }
-            
+        answer[1] = n * m / gcd;
+        
         return answer;
     }
+    
+    private static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        if (a%b == 0)
+            return b;
+        
+        int tmp = a;
+        a = b;
+        b = tmp%b;
+        
+        return gcd(a, b);
+    }
 }
-
-/*
-m % n == 0일 경우 최대공약수는 n
-0이 아닐 경우 n의 약수들을 구해서 작은 수부터 차례대로
-t라고 하자. 이 때 m%t == 0이면 최대공약수는 t이다.
-
-m의 약수들을 구하고 약수 중에서 작은 수부터 차례대로
-n에 곱한 수를 k라고 하자.
-이 때 k%m==0 && k%n==0이면 k는 최소공배수이다.
-*/
