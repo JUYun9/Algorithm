43349b97bf534980cfe9784552617bb1da95bebf
윤정우 <124337134+JUYun9@users.noreply.github.com>
2026-03-16 15:55:31 +0900

    [level 1] Title: K번째수, Time: 0.49 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,17 +4,19 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
+        int idx = 0;
         for (int i = 0; i < commands.length; i++) {
-            int temporaryLength = commands[i][1] - commands[i][0] + 1;
-            int[] temporary = new int[temporaryLength];
-            for (int j = 0; j < temporaryLength; j++) {
-                temporary[j] = array[commands[i][0] + j - 1];
-            }
+            int start = commands[i][0];
+            int end = commands[i][1];
             
-            Arrays.sort(temporary);
+            int[] tmp = new int[end-start+1];
+            for (int j = 0; j < tmp.length; j++)
+                tmp[j] = array[start-1+j];
             
-            answer[i] = temporary[commands[i][2]-1];
+            Arrays.sort(tmp);
             
+            int targetIdx = commands[i][2] - 1;
+            answer[idx++] = tmp[targetIdx];
         }
         
         return answer;
