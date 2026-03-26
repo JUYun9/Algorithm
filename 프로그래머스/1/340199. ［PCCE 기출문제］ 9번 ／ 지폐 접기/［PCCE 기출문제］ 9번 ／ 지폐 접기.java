8d544e8df67a10014793d9eecd5780a95fedb371
윤정우 <124337134+JUYun9@users.noreply.github.com>
2026-03-26 17:13:16 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.07 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
@@ -2,35 +2,15 @@ class Solution {
     public int solution(int[] wallet, int[] bill) {
         int answer = 0;
         
-        
-        int a = Math.max(wallet[0], wallet[1]);
-        int b = Math.min(wallet[0], wallet[1]);
-        
-        int big = Math.max(bill[0], bill[1]);
-        int small = Math.min(bill[0], bill[1]);
-        while (true) {
-            if (big < small) {
-                int tmp = big;
-                big = small;
-                small = tmp;
-            }
-            
-            // System.out.println(big);
-            // System.out.println(small);
-            
-            if (big <= a && small <= b)
-                return answer;
-            
-            big /= 2;
+        while (Math.min(bill[0], bill[1]) > Math.min(wallet[0], wallet[1])
+              || Math.max(bill[0], bill[1]) > Math.max(wallet[0], wallet[1])) {
+            if (bill[0] > bill[1])
+                bill[0] /= 2;
+            else
+                bill[1] /= 2;
             answer++;
         }
+        
+        return answer;
     }
 }
-
-// 23분 ~
-// bill에서 큰 값을 big, 작은 값을 small이라고 하자
-// 반복문을 돌면서
-    // if (wallet의 큰 값 > big && wallet의 작은 값 > small)
-        // return answer
-    // big /= 2, small /= 2
-    // answer++;
