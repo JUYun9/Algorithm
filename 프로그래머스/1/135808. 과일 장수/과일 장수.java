5145598c3cac86514e6eda607f6f88f936945696
윤정우 <124337134+JUYun9@users.noreply.github.com>
2026-03-26 16:28:03 +0900

    [level 1] Title: 과일 장수, Time: 73.88 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -4,24 +4,25 @@ class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
         
-        int num = k;
+        Arrays.sort(score);
+        
+        int q = 1;   // 묶음인데 이거는 필요없을듯
         int count = 0;
-        while (true) {
-            for (int i = 0; i < score.length; i++) {
-                if (score[i] == num)
-                    count++;
-                
-                if (count == m) {
-                    answer += num * count;
-                    count = 0;
-                }
+        for (int i = score.length-1; i >= 0; i--) {
+            count++;
+            if (count == m) {
+                answer += score[i] * m;
+                count = 0;
             }
-            
-            num--;
-            if (num == 0)
-                break;
         }
+        
+        
         
         return answer;
     }
 }
+
+
+// 사과는 상자 단위로만 판매한다.
+// 사과의 점수는 1점 ~ k점
+// m개씩 담는다.
