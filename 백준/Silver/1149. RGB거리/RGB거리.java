8ed1e589a3b2d72c5b43c7d054ba30488acfa112
윤정우 <124337134+JUYun9@users.noreply.github.com>
2026-04-01 15:50:24 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 14556 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -5,37 +5,29 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    private static int[][] dp;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int num = Integer.parseInt(br.readLine());
-        dp = new int[num+1][4];
+        int n = Integer.parseInt(br.readLine());
 
-        int[][] arr = new int[num+1][4];
-        for (int i = 1; i <= num; i++) {
+        int[][] expense = new int[n][3];
+        for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= 3; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
+            for (int j = 0; j < 3; j++)
+                expense[i][j] = Integer.parseInt(st.nextToken());
         }
 
-        // 초기값
-        dp[1][1] = arr[1][1];
-        dp[1][2] = arr[1][2];
-        dp[1][3] = arr[1][3];
-
-        for (int i = 2; i <= num; i++) {
-            dp[i][1] = Math.min(dp[i-1][2], dp[i-1][3]) + arr[i][1];
-            dp[i][2] = Math.min(dp[i-1][1], dp[i-1][3]) + arr[i][2];
-            dp[i][3] = Math.min(dp[i-1][1], dp[i-1][2]) + arr[i][3];
+        int[][] dp = new int[n][3];
+        dp[0][0] = expense[0][0]; dp[0][1] = expense[0][1]; dp[0][2] = expense[0][2];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + expense[i][0];
+            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + expense[i][1];
+            dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + expense[i][2];
         }
 
-        int ans = Integer.MAX_VALUE;
-        for (int i = 1; i <= 3; i++)
-            ans = Math.min(ans, dp[num][i]);
+        int min = Math.min(dp[n-1][0], Math.min(dp[n-1][1], dp[n-1][2]));
 
-        System.out.println(ans);
+        System.out.println(min);
+
     }
 }
