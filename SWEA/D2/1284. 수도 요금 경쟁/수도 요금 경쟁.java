463f3d5ad82209ba98b9deab9b8c383d3754b43a
윤정우 <124337134+JUYun9@users.noreply.github.com>
2026-08-01 16:15:56 +0900

    [D2] Title: 수도 요금 경쟁, Time: 74 ms, Memory: 25,472 KB -BaekjoonHub

---
+++ SWEA/D2/1284. 수도 요금 경쟁/수도 요금 경쟁.java
@@ -0,0 +1,32 @@
+import java.lang.*;
+import java.io.*;
+import java.util.StringTokenizer;
+
+class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int resultA = 0; int resultB = 0;
+            int P = Integer.parseInt(st.nextToken());   // A사 L당 요금
+            int Q = Integer.parseInt(st.nextToken());   // B사 R리터 이하 요금
+            int R = Integer.parseInt(st.nextToken());   // B사 사용한 수도 R리터
+            int S = Integer.parseInt(st.nextToken());   // B사 L당 요금
+            int W = Integer.parseInt(st.nextToken());   // 사용한 수도의 양
+
+            resultA += P*W;
+
+            if (W < R)
+                resultB = Q;
+            else
+                resultB += Q + (W-R)*S;
+            
+            int result = Math.min(resultA, resultB);
+            
+            System.out.println("#" + i + " " + result);
+        }
+    }
+}
