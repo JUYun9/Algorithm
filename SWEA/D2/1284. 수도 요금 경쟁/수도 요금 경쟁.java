3945b4a94b4c83877aa09c9b12ea622e432d4b5c
윤정우 <124337134+JUYun9@users.noreply.github.com>
2026-08-01 16:17:10 +0900

    [D2] Title: 수도 요금 경쟁, Time: 72 ms, Memory: 22,400 KB -BaekjoonHub

--- SWEA/D2/1284. 수도 요금 경쟁/수도 요금 경쟁.java
+++ SWEA/D2/1284. 수도 요금 경쟁/수도 요금 경쟁.java
@@ -24,6 +24,7 @@ class Solution {
             else
                 resultB += Q + (W-R)*S;
             
+            
             int result = Math.min(resultA, resultB);
             
             System.out.println("#" + i + " " + result);
