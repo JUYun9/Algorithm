ee54e9ba7e79e7b4db91337e8f3a0a4dd8a1961a
윤정우 <124337134+JUYun9@users.noreply.github.com>
2026-08-07 09:40:22 +0900

    [level 2] Title: H-Index, Time: 8.96 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        
+        int[] arr = new int[10001];
+        
+        int maxNum = -1;
+        for (int i = 0; i < citations.length; i++) {
+            int target = citations[i];
+            if (target > maxNum)
+                maxNum = target;
+            for (int j = 0; j <= target; j++)
+                arr[j]++;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i < arr[i])
+                answer = i;
+            else if (arr[i] == i)
+                return i;
+            else
+                return answer;
+        }
+        
+        return answer;
+    }
+}
