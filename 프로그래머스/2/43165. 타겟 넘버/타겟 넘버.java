e00fe06f475466b15cbea746166e764cf3ce383b
윤정우 <124337134+JUYun9@users.noreply.github.com>
2026-08-11 11:57:51 +0900

    [level 2] Title: 타겟 넘버, Time: 6.85 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,34 +1,32 @@
-import java.util.*;
-
 class Solution {
-    public int solution(int[] numbers, int target) {
-        int answer = 0;
-        
-        Queue<A> q = new LinkedList<>();
-        
-        q.add(new A(0, 0));
-        
-        while (!q.isEmpty()) {
-            A cur = q.poll();
-            if (cur.idx == numbers.length) {
-                if (cur.sum == target)
-                    answer++;
-                continue;
-            }
-            
-            q.add(new A(cur.idx+1, cur.sum+numbers[cur.idx]));
-            q.add(new A(cur.idx+1, cur.sum-numbers[cur.idx]));
+    static int[] sumArray;
+    static int answer;
+    public int solution(int[] numbers, int target){
+        answer = 0;
+        sumArray = new int[numbers.length];
+        sumArray[numbers.length - 1] = numbers[numbers.length - 1];
+
+        for(int i = numbers.length - 2; i >= 0; i--){ //가지치기 용 뒤에서부터 누적합
+            sumArray[i] = sumArray[i + 1] + numbers[i];
         }
-        
+        dfs(0, numbers, 0, target);
+
         return answer;
     }
-}
 
-class A {
-    int idx;
-    int sum;
-    A(int idx, int sum) {
-        this.idx = idx;
-        this.sum = sum;
+    public void dfs(int depth, int[] numbers, int val, int target){
+        if(depth == numbers.length){
+            if(val == target){
+                answer++;
+            }
+            return;
+        }
+
+        // if(Math.abs(val - target) > sumArray[depth]){ //총 이동해야 하는 거리가 더 클 경우 가지치기
+        //     return;
+        // }
+
+        dfs(depth + 1, numbers, val + numbers[depth], target);
+        dfs(depth + 1, numbers, val - numbers[depth], target);
     }
 }
