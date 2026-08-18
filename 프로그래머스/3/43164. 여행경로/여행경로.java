418fab9550edfc74bbf372366132613114804c84
윤정우 <124337134+JUYun9@users.noreply.github.com>
2026-08-18 10:11:06 +0900

    [level 3] Title: 여행경로, Time: 0.65 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+    
+    private static List<String> ans;
+    private static boolean[] vis;
+    
+    public String[] solution(String[][] tickets) {
+
+        Arrays.sort(tickets, (a, b) -> a[1].compareTo(b[1]));
+        
+        vis = new boolean[tickets.length];
+        ans = new ArrayList<>();
+        
+        ans.add("ICN");
+        
+        boolean b = dfs("ICN", tickets);
+        
+        String[] answer = new String[ans.size()]; 
+        for (int i = 0; i < answer.length; i++)
+            answer[i] = ans.get(i);
+        
+        return answer;
+    }
+    
+    public boolean dfs(String cur, String[][] tickets) {
+        if (ans.size() == tickets.length+1)
+            return true;
+        
+        for (int i = 0; i < tickets.length; i++) {
+            String departure = tickets[i][0];
+            String destination = tickets[i][1];
+            if (!vis[i] && cur.equals(departure)) {
+                vis[i] = true;
+                ans.add(destination);
+                
+                if (dfs(destination, tickets))
+                    return true;
+                
+                vis[i] = false;
+                ans.remove(ans.size()-1);
+            }
+        }
+        
+        return false;
+    }
+}
+
+// 14:53 ~
